588aea31d32547baf4d2951e2cadb909fc1a44e3
bunnyrdy <121040061+bunnyrdy@users.noreply.github.com>
2026-05-09 09:58:07 +0530

    python

--- vars/pythonPYPipeline.groovy
+++ vars/pythonPYPipeline.groovy
@@ -85,6 +85,7 @@ def call(Map configMap){
                    build job: "../${component}-cd", parameters: [string(name: 'version', value: "${appVersion}")], wait: true
                 }
             }
+        }
 
             post {
                 always {
@@ -92,15 +93,15 @@ def call(Map configMap){
                     deleteDir()
                 }
 
-            }
+            
             failure { 
                 echo 'I will run when pipeline is failed'
             }
             success { 
                 echo 'I will run when pipeline is success'
             }
+            }
             
 
         }
     }
-}
